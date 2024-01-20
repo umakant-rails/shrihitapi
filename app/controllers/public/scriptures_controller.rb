8707ea48b03e7614b186b655d338ced8b34acfec
umakant-rails <rajpootumakant@gmail.com>
2024-01-20 10:10:05 +0530

    get scripture articles

--- app/controllers/public/scriptures_controller.rb
+++ app/controllers/public/scriptures_controller.rb
@@ -6,7 +6,7 @@ class Public::ScripturesController < ApplicationController
     scriptures_tmp = @scriptures.map do | scripture |
       scripture.attributes.merge({
         scripture_type: scripture.scripture_type.name,
-        author: scripture.author.name
+        author: scripture.author ? scripture.author.name : 'NA'
       })
     end
 
@@ -16,16 +16,35 @@ class Public::ScripturesController < ApplicationController
   end
 
   def show
+    scripture_tmp = {}
+
     @scripture = Scripture.where(name_eng: params[:id].strip).first rescue nil
     if @scripture.present? && @scripture.scripture_type.name == "रसिक वाणी"
       @articles = @scripture.articles.order("index")
+
+      scriptures_tmp = @scripture.attributes.merge({
+        scripture_type: @scripture.scripture_type.name,
+        author: @scripture.author.name,
+        articles: @articles.map do | article |
+          article.attributes.merge({
+            author: article.author.name,
+            article_type: article.article_type.name
+          })
+        end
+      })
     elsif @scripture.present? && @scripture.scripture_type.name == "कथायें"
       @articles = @scripture.stories.order("index")
+      
+      scriptures_tmp = @scripture.attributes.merge({
+        scripture_type: @scripture.scripture_type ? @scripture.scripture_type.name : 'NA',
+        author: @scripture.author ? @scripture.author.name : 'NA',
+        articles: @articles
+      })
     end
 
-    respond_to do |format|
-      format.html {}
-    end
+    render json: {
+      scripture: scriptures_tmp
+    }
   end
 
 end
