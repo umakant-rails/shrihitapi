14dd6a4827a3f2c9c7ef91bca471ab22be251e20
umakant-rails <rajpootumakant@gmail.com>
2024-01-31 22:36:08 +0530

    add create action in article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,10 @@ class ArticlesController < ApplicationController
     contexts = Context.order("name ASC")
     raags = Raag.order("name ASC")
     article_types = ArticleType.order("name ASC")
-
+    scriptures = Scripture.order("name ASC")
     author = Author.where("name=?", "अज्ञात").first
     context = Context.where("name=?", "अन्य").first
+
     article = Article.new({author_id: author.id, context_id: context.id})
     
     render json: {
@@ -18,8 +19,20 @@ class ArticlesController < ApplicationController
       raags: raags,
       authors: authors,
       article_types: article_types,
+      scriptures: scriptures,
       article: article
     }
+  end
+
+  # POST /articles or /articles.json
+  def create
+    @article = current_user.articles.new(article_params)
+    debugger
+    if true #@article.save
+      redner json: { article: @article, status: "Article was successfully created."}
+    else
+      render json: { article: @article.errors, status: :unprocessable_entity }
+    end
   end
 
 end
