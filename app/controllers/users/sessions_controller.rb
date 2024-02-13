00433ab8ca01102a12e5cbd971a204d87192dac2
umakant-rails <rajpootumakant@gmail.com>
2024-02-13 08:59:53 +0530

    fix session controller response

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -31,7 +31,7 @@ class Users::SessionsController < Devise::SessionsController
     if resource
       render json: {
         status: {code: 200, message: 'Logged in sucessfully.'},
-        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+        user: UserSerializer.new(resource).serializable_hash[:data][:attributes]
       }, status: :ok
     end
   end
