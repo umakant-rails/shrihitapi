bcfb36e820142727fe06743d7f431276586dbe26
umakant-rails <rajpootumakant@gmail.com>
2024-03-02 16:00:22 +0530

    add delete action for scripture article

--- app/controllers/admin/scripture_articles_controller.rb
+++ app/controllers/admin/scripture_articles_controller.rb
@@ -7,14 +7,7 @@
   def index
     page = params[:page].present? ? params[:page] : 1
     
-    if params[:chapter_id].present?
-      @chapter = Chapter.find(params[:chapter_id]);
-      @articles = @chapter.scripture_articles.order("index ASC").page(page).per(10)
-      @total_articles = @chapter.scripture_articles.count
-    else
-      @articles = @scripture.scripture_articles.page(page).per(10)
-      @total_articles = @scripture.scripture_articles.count
-    end
+    get_page_by_articles(page)
 
     render json: {
       articles: @articles,
@@ -80,53 +73,58 @@
   end
 
   def destroy
+    page = params[:page].present? ? params[:page] : 1
     @scripture_article.destroy
 
+    get_page_by_articles(page)
+
     render json: {
-      scripture_article: @scripture_article,
+      articles: @articles,
+      total_articles: @total_articles,
+      current_page: page,
       notice: "Scripture Article is deleted successfully."
     }
   end
 
-  def edit_article_index
-    @scriptures = Scripture.all
-  end
+  # def edit_article_index
+  #   @scriptures = Scripture.all
+  # end
 
-  def update_article_index
-    @scripture_article = ScriptureArticle.find(params[:article_id])
-    respond_to do |format|
-      if @scripture_article.update({index: params[:article_index]})
-        format.html { }
-        format.js { render status: 200 }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @scripture_article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update_article_index
+  #   @scripture_article = ScriptureArticle.find(params[:article_id])
+  #   respond_to do |format|
+  #     if @scripture_article.update({index: params[:article_index]})
+  #       format.html { }
+  #       format.js { render status: 200 }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @scripture_article.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  def get_chapters
-    @scripture = Scripture.find(params[:scripture_id]) rescue nil
-    @chapters = @scripture.present? ? @scripture.chapters : nil
-    @last_article = @scripture.scripture_articles.order("index ASC").last rescue nil
-  end
+  # def get_chapters
+  #   @scripture = Scripture.find(params[:scripture_id]) rescue nil
+  #   @chapters = @scripture.present? ? @scripture.chapters : nil
+  #   @last_article = @scripture.scripture_articles.order("index ASC").last rescue nil
+  # end
 
-  def get_chapter_articles
+  # def get_chapter_articles
     
-    if params[:scripture_id].present?
-      @scripture = Scripture.find(params[:scripture_id]) rescue nil
-      @chapters = @scripture.present? ? @scripture.chapters : nil
-      @articles = @chapters.blank? ? @scripture.scripture_articles.order("index ASC").page(params[:page]) : nil
-    else
-      @chapter = Chapter.find(params[:chapter_id])
-      @articles = @chapter.scripture_articles.order("index ASC").page(params[:page])
-    end
-  end
+  #   if params[:scripture_id].present?
+  #     @scripture = Scripture.find(params[:scripture_id]) rescue nil
+  #     @chapters = @scripture.present? ? @scripture.chapters : nil
+  #     @articles = @chapters.blank? ? @scripture.scripture_articles.order("index ASC").page(params[:page]) : nil
+  #   else
+  #     @chapter = Chapter.find(params[:chapter_id])
+  #     @articles = @chapter.scripture_articles.order("index ASC").page(params[:page])
+  #   end
+  # end
 
-  def get_index
-    @chapter = Chapter.find(params[:chapter_id])
-    @last_article = @chapter.scripture_articles.order("index ASC").last rescue nil
-  end
+  # def get_index
+  #   @chapter = Chapter.find(params[:chapter_id])
+  #   @last_article = @chapter.scripture_articles.order("index ASC").last rescue nil
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.    
@@ -140,6 +138,17 @@
     def scripture_article_params
       params.fetch(:scripture_article, {}).permit(:scripture_id, :article_type_id, :content, :content_eng, 
         :interpretation, :interpretation_eng, :chapter_id, :index)
+    end
+
+    def get_page_by_articles(page)
+      if params[:chapter_id].present?
+        @chapter = Chapter.find(params[:chapter_id]);
+        @articles = @chapter.scripture_articles.order("index ASC").page(page).per(10)
+        @total_articles = @chapter.scripture_articles.count
+      else
+        @articles = @scripture.scripture_articles.page(page).per(10)
+        @total_articles = @scripture.scripture_articles.count
+      end
     end
 
     def verify_admin
