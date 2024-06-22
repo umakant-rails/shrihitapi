5d28d67a049a74b67effc2cb0e1c113733cb91c6
umakant-rails <rajpootumakant@gmail.com>
2024-06-22 23:03:22 +0530

    fix aciton mailer error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,12 +53,13 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 25,
+    port:                 587,
+    domain:               "mail.google.com",
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
