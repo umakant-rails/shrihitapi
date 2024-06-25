51bf10cf96aad13f73f1bd2404f20b5d6cdddb39
umakant-rails <rajpootumakant@gmail.com>
2024-06-25 15:59:35 +0530

    update in capistrano files

--- Capfile
+++ Capfile
@@ -27,9 +27,9 @@ install_plugin Capistrano::SCM::Git
 #   https://github.com/capistrano/passenger
 #
 require "capistrano/rvm"
+require "capistrano/rails"
 # require "capistrano/rbenv"
 # require "capistrano/chruby"
-require "capistrano/rails"
 require "capistrano/bundler"
 require "capistrano/rails/assets"
 require "capistrano/rails/migrations"
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server env['SHRIHITAPI_IP'], user: "deploy", roles: %w{app db web}, my_property: :true
+server "", user: "deploy", roles: %w{app db web}, primary: :true
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
@@ -42,7 +42,7 @@ set :deploy_to, "/home/deploy/apps/shrihitapi"
 # Global options
 # --------------
  set :ssh_options, {
-   # keys: %w(/home/user_name/.ssh/id_rsa),
+   keys: %w(/home/deploy/.ssh/id_rsa),
    forward_agent: false,
    auth_methods: %w(password)
  }
