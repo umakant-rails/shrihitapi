640c3d0eadee9e1e42850c31b9f725759b86347d
umakant-rails <rajpootumakant@gmail.com>
2024-06-27 20:49:06 +0530

    remove asset precompile false

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,5 @@ module Shrihitapi
     config.session_store :cookie_store #, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
-    config.assets.initialize_on_precompile = false 
   end
 end
