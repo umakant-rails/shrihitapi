8c6456b9074047f79c2922401214ffbd3044ef76
umakant-rails <rajpootumakant@gmail.com>
2024-06-30 18:44:07 +0530

    update deploy.rb

--- Capfile
+++ Capfile
@@ -27,7 +27,7 @@ install_plugin Capistrano::SCM::Git
 #   https://github.com/capistrano/passenger
 #
 require "capistrano/rvm"
-require "capistrano/rails"
+# require "capistrano/rails"
 # require "capistrano/rbenv"
 # require "capistrano/chruby"
 require "capistrano/bundler"
--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, ""
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/apps/shrihitapi"
+set :deploy_to, "/home/deploy/apps/shrihitapi"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
