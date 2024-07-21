733013757b37122b3dd2a13b4542061788a533c6
umakant-rails <rajpootumakant@gmail.com>
2024-07-21 13:19:03 +0530

    update public story's show page

--- app/controllers/public/stories_controller.rb
+++ app/controllers/public/stories_controller.rb
@@ -12,8 +12,17 @@ class Public::StoriesController < ApplicationController
 
   def show
     @story = Story.where(title: params[:id].strip)[0] rescue nil
-    @stories = @story.present? ? Story.where("id not in(?)", @story.id).first(10) : Story.first(10)
-    
+
+    if @story && @story.scripture
+      scripture = @story.scripture
+      story_index = @story.index - 1
+
+      @stories = scripture.stories.where("index >= ? and id not in (?)", story_index, @story.id).first(10)
+      @story = @story.attributes.merge({scripture: @story.scripture})
+    else
+      @stories = @story.present? ? Story.where("id not in(?)", @story.id).first(10) : Story.first(10)
+    end
+
     render json: {
       story: @story,
       stories: @stories
