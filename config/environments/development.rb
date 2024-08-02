e300eaae562a1dc328a9f7f4a9a73aabd936956f
umakant-rails <rajpootumakant@gmail.com>
2024-08-02 22:26:19 +0530

    update email setting in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -52,20 +52,18 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              "smtp.office365.com",
     port:                 587,
-    domain:               "mail.google.com",
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'plain',
+    domain:               Rails.application.secrets.domain_name,
+    user_name:            Rails.application.secrets.email,
+    password:             Rails.application.secrets.email_password, #ENV.fetch('EMAIL_PASSWORD'),
+    authentication:      'login',
     enable_starttls_auto: true
   }
-
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
